45c9af7a96903891b306e5a0b82410adee142a2a
pyshok03 <hel365948@gmail.com>
2026-02-28 21:45:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
 from config import *
 from logic import *
 
-
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def start(message):
     bot.send_message(
         message.chat.id,
-        wlc_message,
-        reply_markup=get_keyboard()
+        "Расписание на сегодня - покажет уроки на текущий день\nКогда следующий урок - скажет следующий урок",
+        reply_markup=get_button()
     )
 
 @bot.message_handler(func=lambda message: True)
-def handle_buttons(message):
-    if message.text == "Получить расписание":
-        schedule_text = generate_schedule()
-        bot.send_message(
-            message.chat.id,
-            schedule_text
-        )
+def handle(message):
+    if message.text == "Расписание на сегодня":
+        bot.send_message(message.chat.id, get_schedule_text())
+    elif message.text == "Когда следующий урок":
+        bot.send_message(message.chat.id, get_time_to_next_lesson())
     else:
-        bot.send_message(
-            message.chat.id,
-            "Используйте кнопку 'Получить расписание'",
-            reply_markup=get_keyboard()
-        )
+        bot.send_message(message.chat.id, "Используй кнопки ниже")
 
-
-if __name__ == "__main__":
-    bot.infinity_polling()
+bot.infinity_polling()
